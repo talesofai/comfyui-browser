a2e3552f71b0d73faca5eaef9e52f87674b94017
tzwm <git@tzwm.me>
2023-12-02 15:32:29 +0800

    update browser modal

--- web/index.js
+++ web/index.js
@@ -1,17 +1,79 @@
 import { app } from "../../scripts/app.js";
 import { $el, ComfyDialog } from "../../scripts/ui.js";
 
+const browserUrl = "./browser/index.html";
+
 class BrowserDialog extends ComfyDialog {
   constructor() {
     super();
+
+    this.element = $el("div.comfy-modal", {
+      id: "comfy-browser-dialog",
+      parent: document.body,
+      style: {
+        width: "100%",
+        height: "100%",
+        padding: "10px",
+      },
+    }, [
+      $el("div.comfy-modal-content", {
+        style: {
+          width: "inherit",
+          height: "inherit",
+        },
+      }, [
+        $el("div", {
+          $: (e) => (this.textElement = e),
+          style: {
+            height: "inherit",
+          },
+        }),
+        ...this.createButtons(),
+      ]),
+		]);
+  }
+
+  createButtons() {
+    const closeBtn = $el("button", {
+      type: "button",
+      textContent: "Close",
+      onclick: () => this.close(),
+    });
+    const browseBtn = $el("a", {
+      href: browserUrl,
+      target: "_blank",
+    }, [
+      $el("button", {
+        type: "button",
+        textContent: "Browse in new tab",
+      }),
+    ]);
+    return [
+      $el("div", {}, [
+        closeBtn,
+        browseBtn,
+      ]),
+    ];
+  }
+
+  close() {
+    this.element.style.display = "none";
+  }
+
+  show(html) {
+    if (typeof html === "string") {
+      this.textElement.innerHTML = html;
+    } else {
+      this.textElement.replaceChildren(html);
+    }
+    this.element.style.display = "flex";
   }
 }
 
 
-
 app.registerExtension({
-	name: "ComfyUI.Browser",
-	init() {
+  name: "ComfyUI.Browser",
+  init() {
   },
   async setup() {
     const browserDialog = new BrowserDialog();
@@ -21,10 +83,13 @@ app.registerExtension({
         id: "comfyui-browser-button",
         textContent: "Browser",
         onclick: () => {
-          console.log('click browser');
-          var iframe = document.createElement('iframe');
-          iframe.src = './build/index.html';
-          browserDialog.show(iframe);
+          browserDialog.show($el("iframe", {
+            src: browserUrl,
+            style: {
+              width: "100%",
+              height: "inherit",
+            },
+          }));
         },
       })
     );
