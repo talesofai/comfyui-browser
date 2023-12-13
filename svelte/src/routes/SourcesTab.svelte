64ad234651b87b1581a04370d8c930508c0f450c
tzwm <git@tzwm.me>
2023-12-13 19:12:14 +0800

    feat: support to delete a source

--- svelte/src/routes/SourcesTab.svelte
+++ svelte/src/routes/SourcesTab.svelte
@@ -31,8 +31,28 @@
     await refreshSources();
   });
 
-  function onClickSource(source: any) {
+  function onClickAddSource(source: any) {
     selectedSource = source;
+  }
+
+  async function onClickDeleteSource(source: any) {
+    const ret = confirm('You want to delete this source? ' + source.name);
+    if (!ret) {
+      return;
+    }
+
+    const res = await fetch(comfyUrl + `/browser/sources/${source.name}`, {
+      method: 'DELETE',
+    });
+
+    if (res.ok) {
+      await refreshSources();
+    }
+    toast.show(
+      res.ok,
+      'Deleted this source',
+      'Failed to delete this source. Please check the ComfyUI server.',
+    );
   }
 
   function openEditModal() {
@@ -77,9 +97,17 @@
     {#each sources as source}
       <li>
         <button
-          class="btn"
-          on:click={() => onClickSource(source)}
+          class="btn inline-block w-3/4"
+          on:click={() => onClickAddSource(source)}
         >{source.name}</button>
+        <button
+          class="btn inline-block w-1/4"
+          on:click={() => onClickDeleteSource(source)}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+          </svg>
+        </button>
       </li>
     {/each}
   </ul>
