bd107eafc4ec818e016c3e20ca974f7d99abebd3
tzwm <git@tzwm.me>
2023-12-13 16:59:00 +0800

    feat: support to add new source

--- svelte/src/routes/SourcesTab.svelte
+++ svelte/src/routes/SourcesTab.svelte
@@ -8,25 +8,76 @@
   let sources: Array<any> = [];
   let toast: Toast;
   let selectedSource: any;
+  let sourceEditModal: any;
+  let inputRepoUrl: string;
+  let addWaiting = false;
 
-  onMount(async () => {
+  async function refreshSources(selectGitUrl: string | null = null) {
     const res = await fetch(comfyUrl + '/browser/sources');
     const ret = await res.json();
     sources = ret.sources;
-    selectedSource = sources[0];
+
+    if (selectGitUrl) {
+      selectGitUrl = selectGitUrl.trim();
+      selectedSource = sources.find(s => s.url == selectGitUrl);
+    }
+
+    if (! selectedSource) {
+      selectedSource = sources[0];
+    }
+  }
+
+  onMount(async () => {
+    await refreshSources();
   });
 
   function onClickSource(source: any) {
     selectedSource = source;
   }
+
+  function openEditModal() {
+    sourceEditModal.show();
+  }
+
+  async function addSource() {
+    if (! inputRepoUrl) {
+      toast.show(false, '', 'missing Git URL', 1000);
+      return;
+    }
+
+    addWaiting = true;
+    const res = await fetch(comfyUrl + '/browser/sources', {
+      method: 'POST',
+      body: JSON.stringify({
+        repo_url: inputRepoUrl,
+      }),
+    });
+    addWaiting = false;
+
+    if (res.ok) {
+      await refreshSources(inputRepoUrl);
+    }
+    toast.show(
+      res.ok,
+      'Added a new source',
+      'Failed to add a new source. Please check the ComfyUI server.',
+    );
+    sourceEditModal.close();
+  }
 </script>
 
 <div>
   <ul class="inline-block menu bg-base-200 w-56 p-0 [&_li>*]:rounded-none">
+    <li>
+      <button
+        class="btn"
+        on:click={openEditModal}
+      >Add new source</button>
+    </li>
     {#each sources as source}
       <li>
         <button
-          class="btn text-left"
+          class="btn"
           on:click={() => onClickSource(source)}
         >{source.name}</button>
       </li>
@@ -44,3 +95,23 @@
 </div>
 
 <Toast bind:this={toast} />
+
+<!-- Open the modal using ID.showModal() method -->
+<dialog class="modal" bind:this={sourceEditModal}>
+  <div class="modal-box">
+    <input
+      type="text"
+      bind:value={inputRepoUrl}
+      placeholder="Input Git remote address of the source"
+      class="input input-bordered w-full max-w-xs"
+    />
+    <button
+      class="btn"
+      on:click={addSource}
+      disabled={addWaiting}
+    >{addWaiting ? 'Adding...' : 'Add'}</button>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
