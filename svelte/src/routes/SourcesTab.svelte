6c37b54410c5abc8bc030ce86c8fe9a11434d7ae
tzwm <git@tzwm.me>
2023-12-15 14:46:19 +0800

    add sync button to Sources#index

--- svelte/src/routes/SourcesTab.svelte
+++ svelte/src/routes/SourcesTab.svelte
@@ -13,6 +13,7 @@
   let folderPath = '';
   let allSources: Array<any> = [];
   let fileList: FilesList;
+  let syncingSouce: any;
 
   async function refreshSources() {
     const res = await fetch(comfyUrl + '/browser/sources');
@@ -48,6 +49,24 @@
       res.ok,
       'Deleted this source',
       'Failed to delete this source. Please check the ComfyUI server.',
+    );
+  }
+
+  async function onClickSyncSource(source: any) {
+    syncingSouce = source;
+    const res = await fetch(comfyUrl + `/browser/sources/sync/${source.name}`, {
+      method: 'POST',
+    });
+    syncingSouce = null;
+
+    if (res.ok) {
+      refreshSources();
+      fileList.refresh();
+    }
+    toast.show(
+      res.ok,
+      'Synced ' + [source.author, source.name].join('/'),
+      'Failed to sync this source. Please check the ComfyUI server.',
     );
   }
 
@@ -115,10 +134,11 @@
           ><p class="w-40 truncate whitespace-normal">{source.name}</p></button>
           <button
             class="h-14 right-0 fixed flex items-center justify-center"
-            on:click={() => onClickDeleteSource(source)}
+            on:click={() => onClickSyncSource(source)}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+              class="{syncingSouce == source ? 'text-accent' : ''} w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
             </svg>
           </button>
         </li>
