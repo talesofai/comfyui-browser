e209fbb4fd530c478d1a29cfd087ca0c59270995
tzwm <git@tzwm.me>
2023-12-19 16:00:57 +0800

    Add the link for sources

--- svelte/src/routes/SourcesTab.svelte
+++ svelte/src/routes/SourcesTab.svelte
@@ -18,7 +18,17 @@
   async function refreshSources() {
     const res = await fetch(comfyUrl + '/browser/sources');
     const ret = await res.json();
-    sources = ret.sources;
+    sources = ret.sources.map((s: any) => {
+      s.homepage = '';
+      if (s.url.startsWith('https://')) {
+        s.homepage = s.url;
+        if (s.homepage.endsWith('.git')) {
+          s.homepage = s.homepage.slice(0, -4);
+        }
+      }
+
+      return s;
+    });
   }
 
   onMount(async () => {
@@ -122,14 +132,37 @@
 
   <div class="drawer-side border-r border-base-content pr-2">
     <ul class="menu bg-base-200 w-56 p-0 [&_li>*]:rounded-none">
-      <li>
-        <button class="btn-outline btn-accent" on:click={openEditModal}>Add new source</button>
+      <li  class="h-10">
+        <button class="pl-2 btn-outline btn-accent" on:click={openEditModal}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+          Add new source
+        </button>
       </li>
 
       {#each sources as source}
         <li class="h-14">
+          {#if source.homepage }
+            <a
+              class="h-14 px-0.5 flex items-center justify-center"
+              href={source.homepage}
+              target="_blank"
+            >
+              <!--TODO: using more general icon instead of GitHub-->
+              <svg class="w-6 h-6 fill-accent" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
+                <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+              </svg>
+            </a>
+          {:else}
+            <div class="h-14 px-0.5 flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-6 h-6 text-accent">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z" />
+              </svg>
+            </div>
+          {/if}
           <button
-            class="h-14 line-clamp-2 overflow-hidden {source.name == folderPath.split('/')[0] ? 'active' : ''}"
+            class="pl-2 h-14 line-clamp-2 overflow-hidden {source.name == folderPath.split('/')[0] ? 'active' : ''}"
             on:click={() => onClickSource(source)}
           ><p class="w-40 truncate whitespace-normal">{source.name}</p></button>
           <button
