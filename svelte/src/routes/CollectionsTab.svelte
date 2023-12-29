9eeb84806716d978b08b80390cb63cf6fb0176c2
tzwm <git@tzwm.me>
2023-12-30 00:19:06 +0800

    [i18n] finish CollectionsTab translation

--- svelte/src/routes/CollectionsTab.svelte
+++ svelte/src/routes/CollectionsTab.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { _ } from 'svelte-i18n';
+  import { t } from 'svelte-i18n';
   import { fetchFiles, onLoadWorkflow, onScroll } from './utils';
   import MediaShow from './MediaShow.svelte';
   import Toast from './Toast.svelte';
@@ -54,18 +54,18 @@
   async function onClickSyncCollections(e: Event) {
     const btn = e.target as HTMLButtonElement;
     btn.disabled = true;
-    btn.innerHTML = 'Syncing...';
+    btn.innerHTML = $t('collectionsTab.btn.syncing');
     const res = await fetch(comfyUrl + '/browser/collections/sync', {
       method: 'POST',
     });
 
     toast.show(
       res.ok,
-      'Synced',
-      'Failed to sync. Please check the ComfyUI server.',
+      $t('collectionsTab.toast.synced'),
+      $t('collectionsTab.toast.syncFailed'),
     );
     btn.disabled = false;
-    btn.innerHTML = 'Sync';
+    btn.innerHTML = $t('collectionsTab.btn.sync');
 
     folderPath = '';
     files = await fetchFiles(folderType, comfyUrl);
@@ -81,13 +81,13 @@
 
     toast.show(
       res.ok,
-      'Updated config',
-      'Failed to update config. Please check the ComfyUI server.',
+      $t('collectionsTab.toast.configUpdated'),
+      $t('collectionsTab.toast.configUpdatedFailed'),
     );
   }
 
   async function onDelete(file: any) {
-    const ret = confirm('You want to delete this file? ' + file.name);
+    const ret = confirm($t('collectionsTab.toast.deleteConfirm') + file.name);
     if (!ret) {
       return;
     }
@@ -103,8 +103,8 @@
 
     toast.show(
       res.ok,
-      'Deleted the file ' + file.name,
-      'Failed to delete the file. Please check the ComfyUI server.',
+      $t('collectionsTab.toast.deleteSuccess') + file.name,
+      $t('collectionsTab.toast.deleteFailed'),
     );
     files = files.filter((f) => f != file);
   }
@@ -138,8 +138,8 @@
 
     toast.show(
       ret,
-      'Memos Updated',
-      'Failed to update. Please check the ComfyUI server.',
+      $t('collectionsTab.toast.updated'),
+      $t('collectionsTab.toast.updatedFailed'),
     );
   }
 
@@ -158,8 +158,8 @@
 
     toast.show(
       ret,
-      'Updated',
-      'Failed to update. Please check the ComfyUI server.',
+      $t('collectionsTab.toast.updated'),
+      $t('collectionsTab.toast.updatedFailed'),
     );
   }
 
@@ -204,24 +204,24 @@
   </a>
   <input
     type="text"
-    placeholder={$_('collection.syncInput.placeholder')}
+    placeholder={$t('collectionsTab.syncInput.placeholder')}
     bind:value={configGitRepo}
     class="input input-bordered w-full max-w-lg"
   />
   {#if configGitRepo != config?.git_repo}
     <button class="btn btn-outline btn-accent" on:click={onClickSaveConfig}>
-      {$_('collection.btn.save')}
+      {$t('collectionsTab.btn.save')}
     </button>
   {/if}
   <button class="btn btn-outline btn-accent" on:click={onClickSyncCollections}>
-    {$_('collection.btn.sync')}
+    {$t('collectionsTab.btn.sync')}
   </button>
 </div>
 
 <div class="max-w-full text-sm breadcrumbs flex flex-row ml-4">
   <ul class="basis-2/3">
     <li>
-      <button on:click={() => onClickPath(-1)}>{$_('common.rootDir')}</button>
+      <button on:click={() => onClickPath(-1)}>{$t('common.rootDir')}</button>
     </li>
     {#each (folderPath || '').split('/') as path, index}
       <li><button on:click={() => onClickPath(index)}>{path}</button></li>
@@ -230,7 +230,7 @@
 
   <input
     type="text"
-    placeholder={$_('collection.searchInput.placeholder')}
+    placeholder={$t('collectionsTab.searchInput.placeholder')}
     bind:value={searchQuery}
     class="input input-bordered border-slate-600 w-full h-full rounded-none text-sm basis-1/3"
   />
@@ -258,7 +258,7 @@
             <button
               class="btn btn-link btn-sm p-0 no-underline text-accent"
               on:click={async () => await onLoadWorkflow(file, comfyApp, toast)}
-              >Load</button
+              >{$t('common.btn.load')}</button
             >
           {/if}
           <button
@@ -268,7 +268,7 @@
             <svg class="w-3 h-3" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M16 2v4h6v2h-2v14H4V8H2V6h6V2h8zm-2 2h-4v2h4V4zm0 4H6v12h12V8h-4zm-5 2h2v8H9v-8zm6 0h-2v8h2v-8z" fill="#f77"/>
             </svg>
-            Remove
+            {$t('common.btn.delete')}
         </button>
         </div>
       </div>
@@ -276,7 +276,7 @@
       <div>
         <textarea
           name="notes"
-          placeholder="write some memos..."
+          placeholder={$t('collectionsTab.collection.memoPlaceholder')}
           on:blur={(e) => updateFileNotes(e, file)}
           class="resize-none textarea hidden md:block md:w-72 max-w-72 h-14 sm:h-24"
           value={file.notes}
@@ -290,9 +290,9 @@
   <div class="w-full h-full flex items-center justify-center">
     <span class="font-bold text-4xl">
       {#if loaded}
-        {$_('common.emptyList')}
+        {$t('common.emptyList')}
       {:else}
-        {$_('common.rootDir')}
+        {$t('common.rootDir')}
       {/if}
     </span>
   </div>
