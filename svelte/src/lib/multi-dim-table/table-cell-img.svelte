27be9e53a06e10265b9d036adfb941c472903282
miaobuao <miaobuao@outlook.com>
2024-03-21 15:43:25 +0800

    update status position

--- svelte/src/lib/multi-dim-table/table-cell-img.svelte
+++ svelte/src/lib/multi-dim-table/table-cell-img.svelte
@@ -16,7 +16,7 @@
 
   mode.subscribe((v) => {
     _mode = v;
-    if (v === TableMode.Score)
+    if (v === TableMode.Score && value.uuid)
       db.scoreboard
         .where('uuid')
         .equals(value.uuid)
@@ -99,37 +99,16 @@
   />
   {#if _mode === TableMode.Score}
     <button
-      class="absolute h-full w-full top-0 left-0 my-img-mask text-white flex flex-col justify-center items-center"
+      class="absolute w-full h-full top-0 left-0 flex justify-end items-end p-1"
       tabindex="0"
       on:click={like ? onDislike : onLike}
     >
-      <div class="flex justify-around btn-container">
-        <svg class="inline-block w-1/3" viewBox="0 0 1024 1024">
-          <path
-            d="M725.333333 192c-89.6 0-168.533333 44.8-213.333333 115.2C467.2 236.8 388.266667 192 298.666667 192 157.866667 192 42.666667 307.2 42.666667 448c0 253.866667 469.333333 512 469.333333 512s469.333333-256 469.333333-512c0-140.8-115.2-256-256-256z"
-            fill={like ? '#F44336' : 'grey'}
-          />
-        </svg>
-      </div>
+      <svg class="inline-block w-1/4" viewBox="0 0 1024 1024">
+        <path
+          d="M725.333333 192c-89.6 0-168.533333 44.8-213.333333 115.2C467.2 236.8 388.266667 192 298.666667 192 157.866667 192 42.666667 307.2 42.666667 448c0 253.866667 469.333333 512 469.333333 512s469.333333-256 469.333333-512c0-140.8-115.2-256-256-256z"
+          fill={like ? '#F44336' : 'grey'}
+        />
+      </svg>
     </button>
   {/if}
 </div>
-
-<style>
-  @keyframes my-img-blur {
-    0% {
-      backdrop-filter: blur(0px);
-      background: rgba(0, 0, 0, 0);
-    }
-    100% {
-      backdrop-filter: blur(2px);
-      background: rgba(0, 0, 0, 0.5);
-    }
-  }
-  .my-img-mask {
-    animation: my-img-blur 0.15s ease-in both;
-  }
-  .btn-container > * {
-    cursor: pointer;
-  }
-</style>
